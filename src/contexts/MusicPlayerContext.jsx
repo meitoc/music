8c232382e72ff48afbb31e7404aa86d0be5e52c6
meitoc <info@meitoc.com>
2023-07-01 23:30:23 +0700

    music

--- src/contexts/MusicPlayerContext.jsx
+++ src/contexts/MusicPlayerContext.jsx
@@ -4,8 +4,8 @@ const MusicPlayerContext = React.createContext();
 
 import Track1 from "../music/Quan-Ho-Luong-Giac-Minh-Ngoc.mp3"
 import Track2 from "../music/Cong-Thuc-Nhan-Doi-Mai-Ky.mp3"
-import Track3 from "../music/Bai-Rap-Tom-Tat-Ly-Thuyet-Vat-Ly-lop-12-Zarily.mp3"
-import Track4 from "../music/Bai-Rap-Tom-Tat-Ly-Thuyet-Vat-Ly-lop-12-Phan-2-Zarily.mp3"
+import Track3 from "../music/Rap-Vat-Ly1-Zarily.mp3"
+import Track4 from "../music/Rap-Vat-Ly2-Zarily.mp3"
 const defaultValues = {
     audioPlayer: new Audio(),
     tracks:[
